1d226e77d9e3bbd1b115354f2aca90104bf6b423
vishalpokarne <vishalpokarne@gmail.com>
2019-11-22 16:08:55 +0100

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,15 @@
+public class Book {
+
+private int book_id;
+private String book_name;
+
+public boolean addBookItem(int book_id);
+public boolean deleteBookItem(int book_id);
+
+}
+
+public static void main()
+{
+
+
+}
